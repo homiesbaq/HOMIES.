224743dc986893c1476b8357d5dd5b2ccdfc82af
homiesbaq <gabotraslee@gmail.com>
2026-02-01 20:10:02 -0500

    products.js

---
+++ js/products.js
@@ -0,0 +1,33 @@
+const products = [
+  {
+    id: "homies-tee",
+    name: "Homies Tee",
+    price: "$120.000",
+    description: "Camiseta gráfica inspirada en la cultura urbana.",
+    images: [
+      "img/barran.jpg",
+      "img/h2.jpg",
+      "img/h3.jpg"
+    ]
+  },
+  {
+    id: "street-cap",
+    name: "Street Cap",
+    price: "$90.000",
+    description: "Gorra urbana edición limitada.",
+    images: [
+      "img/barranquilla.png",
+      "img/c2.jpg"
+    ]
+  },
+   {
+    id: "tote-black",
+    name: "ToteBags",
+    price: "$800.000",
+    description: "Son lindas y creativas.",
+    images: [
+      "img/c1.jpg",
+      "img/c2.jpg"
+    ]
+  }
+];
